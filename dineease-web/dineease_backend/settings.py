f258c1a5ebc6dcc8b857351a2a3d2ccd3d7f156c
Mico Ang <mico.dahang@gmail.com>
2024-12-03 02:41:37 +0800

    [update] added hosts

--- dineease-web/dineease_backend/settings.py
+++ dineease-web/dineease_backend/settings.py
@@ -28,7 +28,7 @@ STRIPE_SECRET_KEY = ""
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.118.18.181', 'localhost']
+ALLOWED_HOSTS = ['18.118.18.181', 'localhost', 'partners.dineease.ca']
 
 SITE_ID = 1
 
@@ -84,6 +84,8 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://18.118.18.181",
     "https://18.118.18.181",
+    "https://partners.dineease.ca",
+    "http://partners.dineease.ca",
     "http://localhost:3000",
 ]
 
