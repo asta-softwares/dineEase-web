04af3bf28006d32b5851604b01daf6007b0b4ffa
Mico Ang <mico.dahang@gmail.com>
2025-01-03 01:12:05 +0800

    [update] commented out django filters

--- dineease-web/dineease_backend/settings.py
+++ dineease-web/dineease_backend/settings.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
     'mapwidgets',
     'django.contrib.gis',
     'channels',
-    'django_filters',
+    # 'django_filters',
 ]
 
 REST_FRAMEWORK = {
@@ -66,7 +66,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    # 'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
 SIMPLE_JWT = {
