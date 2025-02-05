048c44cb207ff17a960e15719b57af647c897805
Mico Ang <mico.dahang@gmail.com>
2025-02-05 21:12:03 +0800

    [update] added changes

--- dineease-frontend/middleware/auth.global.js
+++ dineease-frontend/middleware/auth.global.js
@@ -12,7 +12,7 @@ export default defineNuxtRouteMiddleware((to) => {
   }
 
   // Redirect to home/dashboard if already authenticated and trying to access the login or register page
-  if ((to.path === '/login' || to.path === '/register' || to.path === '/verify-email' || to.path === '/reset-password', to.path === '/forgot-password') && isAuthenticated()) {
+  if ((to.path === '/login' || to.path === '/register' || to.path === '/verify-email' || to.path === '/reset-password', to.path === '/find-account') && isAuthenticated()) {
     return navigateTo('/') // Redirect to home or dashboard
   }
 })
